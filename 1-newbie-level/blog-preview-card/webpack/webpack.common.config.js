fe8338e87053259659eb3d20d2280607aa73984b
leminhhieu98py <leminhhieu98py@gmail.com>
2025-11-18 00:16:30 +0700

    feat: implement the styling for the blog preview card component

--- 1-newbie-level/blog-preview-card/webpack/webpack.common.config.js
+++ 1-newbie-level/blog-preview-card/webpack/webpack.common.config.js
@@ -46,6 +46,10 @@ const config = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
